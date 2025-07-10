653b04a522488847ed421285cc5ef0fcdfd36499
nandhalabs <nandhab@users.noreply.github.com>
2025-07-10 12:41:13 +0530

    terraform for ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,40 +1,38 @@
 # Security Group
 resource "aws_security_group" "allow_ssh_8080_8000" {
-  name        = "allow-ssh-8080-8000"
-  description = "Allow SSH, 8080, and 8000 inbound traffic"
+name        = "allow-ssh-8080-8000"
+description = "Allow SSH, 8080, and 8000 inbound traffic"
 
-  ingress = [
-    {
-      description = "SSH"
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      description = "Port 8080"
-      from_port   = 8080
-      to_port     = 8080
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      description = "Port 8000"
-      from_port   = 8000
-      to_port     = 8000
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-  ]
+ingress {
+  description = "SSH"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
-  egress = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+ingress {
+  description = "Port 8080"
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+  description = "Port 8000"
+  from_port   = 8000
+  to_port     = 8000
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 }
 
 # Get Amazon Linux 2 AMI
